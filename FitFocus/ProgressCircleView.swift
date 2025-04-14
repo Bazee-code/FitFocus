d897ec0bf87cd692e4eafba1762d460f50225af6
Bazee-code <eugeneobazee2@gmail.com>
2025-04-14 16:50:01 +0300

    Add progress circle view

--- FitFocus/ProgressCircleView.swift
+++ FitFocus/ProgressCircleView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ProgressCircleView: View {
+    @Binding var progress: Int
+    var goal: Int
+    var color: Color
+    private let width: CGFloat = 20
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            ZStack{
+                Circle()
+                    .stroke(color.opacity(0.3), lineWidth: width)
+                
+                Circle()
+                    .trim(from: 0, to: CGFloat(progress) / CGFloat(goal))
+                    .stroke(color, style : StrokeStyle(lineWidth: width, lineCap: .round))
+                    .rotationEffect(.degrees(-90))
+                    .shadow(radius: 5)
+            }
+            .padding()
     }
 }
 
 struct ProgressCircleView_Previews: PreviewProvider {
     static var previews: some View {
-        ProgressCircleView()
+        ProgressCircleView(progress: .constant(100), goal: 200, color: .red)
     }
 }
