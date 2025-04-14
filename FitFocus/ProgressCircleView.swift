898f690e6d59f6dc376645567b416b4a2539c7a3
Bazee-code <eugeneobazee2@gmail.com>
2025-04-14 16:49:20 +0300

    Add header view

---
+++ FitFocus/ProgressCircleView.swift
@@ -0,0 +1,20 @@
+//
+//  ProgressCircleView.swift
+//  FitFocus
+//
+//  Created by Eugene Obazee on 14/04/2025.
+//
+
+import SwiftUI
+
+struct ProgressCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressCircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressCircleView()
+    }
+}
