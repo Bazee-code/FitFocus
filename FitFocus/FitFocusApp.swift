3e9a3615c83efb882529ca6f6473c30a49d23385
Bazee-code <eugeneobazee2@gmail.com>
2025-04-14 15:46:57 +0300

    Initialize project

---
+++ FitFocus/FitFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFocusApp.swift
+//  FitFocus
+//
+//  Created by Eugene Obazee on 14/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
