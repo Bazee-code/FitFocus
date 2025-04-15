b6b45371225dcb345a58bc624bd3a38c36e959cc
Bazee-code <eugeneobazee2@gmail.com>
2025-04-15 12:26:32 +0300

    Add fetchExerciseTime & calories burned functions

--- FitFocus/Managers/HealthManager.swift
+++ FitFocus/Managers/HealthManager.swift
@@ -8,6 +8,14 @@
 import Foundation
 import HealthKit
 
+extension Date {
+    static var startOfDay: Date {
+        let calendar = Calendar.current
+        
+        return calendar.startOfDay(for: Date())
+    }
+}
+
 class HealthManager {
     
     static let shared = HealthManager()
@@ -33,6 +41,48 @@ class HealthManager {
     
     func fetchTodayCaloriesBurned(completion: @escaping(Result<Double, Error>) -> Void){
         let calories = HKQuantityType(.activeEnergyBurned)
-        let predicate = HKQuery.predicateForSamples(withStart: <#T##Date?#>, end: Date())
+        let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
+        let query = HKStatisticsQuery(quantityType: calories, quantitySamplePredicate: predicate) { _, results,
+            error in
+            guard let quantity = results?.sumQuantity(), error == nil else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            let calorieCount = quantity.doubleValue(for: .kilocalorie())
+            completion(.success(calorieCount))
+        }
+        healthStore.execute(query)
     }
+    func fetchTodayExerciseTime(completion: @escaping(Result<Double, Error>) -> Void){
+        let exercise = HKQuantityType(.appleExerciseTime)
+        let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
+        let query = HKStatisticsQuery(quantityType: exercise, quantitySamplePredicate: predicate) { _, results,
+            error in
+            guard let quantity = results?.sumQuantity(), error == nil else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            let exerciseTime = quantity.doubleValue(for: .minute())
+            completion(.success(exerciseTime))
+        }
+        healthStore.execute(query)
+    }
+    func fetchTodayStandTime(completion: @escaping(Result<Double, Error>) -> Void){
+        let standTime = HKQuantityType(.appleStandHour)
+        let predicate = HKQuery.predicateForSamples(withStart: .startOfDay, end: Date())
+        let query = HKStatisticsQuery(quantityType: standTime, quantitySamplePredicate: predicate) { _, results,
+            error in
+            guard let quantity = results?.sumQuantity(), error == nil else {
+                completion(.failure(NSError()))
+                return
+            }
+            
+            let standTime = quantity.doubleValue(for: .minute())
+            completion(.success(standTime))
+        }
+        healthStore.execute(query)
+    }
+    
 }
