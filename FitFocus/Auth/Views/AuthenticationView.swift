55b0fc5bd5a0174c77a6ce7296c15e2cc0c1afb9
Bazee-code <eugeneobazee2@gmail.com>
2025-04-29 12:36:36 +0300

    Add app utility for google sign in

---
+++ FitFocus/Auth/Views/AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  FitFocus
+//
+//  Created by Eugene Obazee on 29/04/2025.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
