b283af3780f2c57e55f50f9a09d27d9efe6ca28d
Bazee-code <eugeneobazee2@gmail.com>
2025-04-29 13:31:41 +0300

    Add google signin & logout functions

---
+++ FitFocus/InitialView.swift
@@ -0,0 +1,18 @@
+//
+//  InitialView.swift
+//  FitFocus
+//
+//  Created by Eugene Obazee on 29/04/2025.
+//
+
+import SwiftUI
+
+struct InitialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InitialView()
+}
