ab07013dbfa6d6c078d70619f07ca875bb6dc3ed
Bazee-code <eugeneobazee2@gmail.com>
2025-04-30 14:24:51 +0300

    Add get current user method

---
+++ FitFocus/Auth/Repositories/AuthRepository.swift
@@ -0,0 +1,18 @@
+//
+//  AuthRepository.swift
+//  FitFocus
+//
+//  Created by Eugene Obazee on 30/04/2025.
+//
+
+import SwiftUI
+
+struct AuthRepository: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthRepository()
+}
